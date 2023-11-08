b3bfdb76d679f43c1a484fc7f95ccbb2d7e80db4
Oleh <oleh.rudenko1@nure.ua>
2023-11-08 20:04:47 +0200

    test commit

--- deploy.php
+++ deploy.php
@@ -23,7 +23,6 @@
 		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
 		$output .= htmlentities(trim($tmp)) . "\n";
 	}
-	// Make it pretty for manual user access (and why not?)
 ?>
 <!DOCTYPE HTML>
 <html lang="en-US">
